96b0f1fdf8270357fb95a73fad2511d1cdd2fd6e
yuukimotai <yuus.saddharma.training@gmail.com>
2023-08-23 19:34:16 +0900

    プロジェクトの作成とプッシュ

---
+++ PDCA-App/PDCA_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDCA_AppApp.swift
+//  PDCA-App
+//
+//  Created by 茂田井雄輝 on 2023/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PDCA_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
