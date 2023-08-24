cfd5f3b77416f15396ce9c5d62d91115b763db03
yuukimotai <yuus.saddharma.training@gmail.com>
2023-08-24 20:35:58 +0900

    実装予定のリストの追加

--- PDCA-App/ContentView.swift
+++ PDCA-App/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("今日、何した？")
+            Text("何ができなかった？")
+            Text("何ができたらよかった？")
+            Text("いつまでにやる？")
         }
         .padding()
     }
